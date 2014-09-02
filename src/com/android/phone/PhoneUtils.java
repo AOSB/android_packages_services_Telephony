lt03lte-cafaosp/packages-services-Telephony

TeleService: Fix issue can't answer/reject call when at USSD content view
When show the USSD dialog, MT a call to the device at this time and
the IncallScreen will show behind the USSD dialog. That's because
the USSD dialog has the WindowManager.LayoutParams.TYPE_SYSTEM_DIALOG
and WindowManager.LayoutParams.FLAG_DIM_BEHIND properties.
So it always show on top of screen.

We should add the function to hide the USSD dialog when a new ringing
connection comes and show it again after disconnect this connection.

CRs-Fixed: 551995

Change-Id: I5ea315c012ecd17f8d0e5e3b36299a80d6f20fc8

src/com/android/phone/PhoneUtils.java
@@ -1251,6 +1251,7 @@ public class PhoneUtils {
                         WindowManager.LayoutParams.FLAG_DIM_BEHIND);
 
                 newDialog.show();
+                app.setUSSDResponseDialog(newDialog);
             } else {
                 if (DBG) log("USSD code has requested user input. Constructing input dialog.");
 

AOSB/android_packages_services_Telephony

src/com/android/phone/PhoneUtils.java
@@ -1342,6 +1342,7 @@ public class PhoneUtils {
                 sUssdMsg.insert(0, text);
                 sUssdDialog.setMessage(sUssdMsg.toString());
                 sUssdDialog.show();
+                app.setUSSDResponseDialog(sUssdDialog);
             } else {
                 if (DBG) log("USSD code has requested user input. Constructing input dialog.");
 